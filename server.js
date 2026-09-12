98cc616527f927ac804a1ec3408cf7ba4039591d
yboguyrogerybo-create <yboguyrogerybo@gmail.com>
2026-09-12 10:49:34 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,12 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const path = require('path');
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log('Serveur sur port ' + PORT);
-});
-
-module.exports = app;
+{
+  "name": "fran-chat",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "socket.io": "^4.7.2"
+  }
+}
