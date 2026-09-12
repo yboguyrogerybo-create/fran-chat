0ab58672cb6956ac9db6297a2c1beba9d6a3483f
yboguyrogerybo-create <yboguyrogerybo@gmail.com>
2026-09-12 10:46:39 +0000

    Refactor server setup and improve socket handling

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-app.get('/', (req,res)=>res.sendFile(__dirname+'/index.html'));
-io.on('connection', s=>{
- s.on('chat message', m=>io.emit('chat message', m));
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
-http.listen(3000, ()=>console.log('FRAN-CHAT OK sur port 3000'));
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log('Serveur sur port ' + PORT);
+});
+
+module.exports = app;
